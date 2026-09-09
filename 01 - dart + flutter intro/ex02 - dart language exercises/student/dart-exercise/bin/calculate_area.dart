4e847e4df7f6d8afaa1deeb5bf619a68a71b4c22
Nattapong-Wararatchai <nwararatchai1@nait.ca>
2026-09-09 12:55:17 -0600

    dart exercise - calculation

---
+++ 01 - dart + flutter intro/ex02 - dart language exercises/student/dart-exercise/bin/calculate_area.dart
@@ -0,0 +1,21 @@
+import "dart:io";
+
+double calculateArea(double length, double width, double height) {
+  return 2 * ((length * width) + (length * height) + (width * height));
+}
+
+double calculateVolume(double length, double width, double height) {
+  return length * width * height;
+}
+
+void main() {
+  print("Gimme the fucking answer");
+  // I need to a) take user unput, and b) immediaely coerce that to a double
+  // because terminal input shows up as a string
+  double length = double.parse(stdin.readLineSync()!);
+  double width = double.parse(stdin.readLineSync()!);
+  double height = double.parse(stdin.readLineSync()!);
+
+  print('Surface Area: ${calculateArea(length, width, height)}');
+  // print('Volume: $
+}
--- 01 - dart + flutter intro/ex02 - dart language exercises/student/dart-exercise/bin/dart_exercise.dart
+++
@@ -1,5 +0,0 @@
-import 'package:dart_exercise/dart_exercise.dart' as dart_exercise;
-
-void main(List<String> arguments) {
-  print('Hello world: ${dart_exercise.calculate()}!');
-}
